ca629bf0a20f0d569d17e789551092acf81dcad6
17865 <17865@10.213.143.132>
2021-02-09 21:23:05 -0500

    Initial Commit of Project Files

---
+++ src/net/codejava/HelloWord.java
@@ -0,0 +1,15 @@
+package net.codejava;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		int test = 0;
+		int piece = 1;
+		while (test < 5) {
+			System.out.print("Piece " + piece +" ; ");
+			piece++;
+			test++;
+		}
+	}
+
+}
